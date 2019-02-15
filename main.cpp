5aaee73a13011c3248b10dbbf6cb4add61d539f1
SanandaB <34948941+SanandaB@users.noreply.github.com>
2019-02-15 10:33:31 -0700

    Added Comments

--- main.cpp
+++ main.cpp
@@ -16,6 +16,9 @@ class Polygon {
     }
 };
 
+/*Base Class Polygon inheritted by classes Square, Triangle and Circle, attributes width and height 
+accessed in sub classes and function area overridden in subclass to compute respective areas.*/
+
 class Square: public Polygon {
   public:
     int area()
@@ -38,15 +41,23 @@ class Circle: public Polygon{
 };
 
 int main () {
+  
+  //Instantiating objects of the four classes
   Square sqr;
   Triangle trgl;
   Circle crcl;
   Polygon * poly1 = &sqr;
   Polygon * poly2 = &trgl;
   Polygon * poly3 = &crcl;
+  
+  /*Setter function to assign user passed values to the shapees. This shows encapsulation as main function does not
+  know which 'setter' function is initializing which shape. */
   poly1->setter (4,5);
   poly2->setter (4,5);
   poly3->setter (4,0);
+  
+  /*Also 'area' method has no arguments and same name for all three classes(Square, Triangle and Circle) but outputs
+  differnt result for differnt shapes by overriding 'area' method from base class Polygon.*/
   cout <<"Square Area:"<< sqr.area() << '\n';
   cout <<"Triangle Area:"<< trgl.area() << '\n';
   cout <<"Circle Area:"<< crcl.area() <<'\n';
